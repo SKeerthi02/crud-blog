0bb573d4c61372d45c57500c3ab58211ff67b563
SKeerthi02 <keerthi.21094@gmail.com>
2021-02-10 18:39:03 -0500

    adding api end points for each CRUD

--- crud-backend/Blog/routes.py
+++ crud-backend/Blog/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from
 from crud-backend import db
 
 blogs= Blueprint('blogs', __name__)
@@ -14,3 +15,41 @@ def create_blog():
 
     blog_id = getattr(new_blog, "id")
     return jsonify({"id": blog_id})
+
+
+@blogs.route('/blogs', methods=['GET'])
+def get_blogs():
+    blogs = Blog.query.all()
+    jsonified_data = []
+    for blog in blogs:
+        jsonified_data.append(blog.jsonify_data)
+    return jsonify({"blogs": jsonified_data})
+
+
+@blogs.route('/blog/<int:db>', methods=['GET'])
+def get_blog():
+    requested_blog = Blog.query.filter_by(id=id).first()
+    jsonified_data = requested_blog.jsonify_data
+    return jsonify({"blogs": jsonified_data})
+
+@blogs.route('/update/<int:id>', methods=['PUT'])
+def update():
+    current = request.get_json()
+    blog = Blog.filter_by(id=id).first_or_404()
+
+    current.blog_title = data['title']
+    current.blog_content = data['content']
+    current.blog_image = data['image']
+
+    update_blog = current.jsonify_data
+
+    db.session.commit()
+    return jsonify({"blog_id":current.id})
+
+@blogs.route('/delete/<int:id>', methods=["Delete"])
+def delete_blog(id):
+    blog = Blog.query.filter_by(id=id).first()
+    db.sesion.delete(blog)
+    db.session.commit()
+
+    return jsonify("Blog was deleted"), 200
--- crud-backend/app.py
+++ crud-backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, url_for, render_template, jsonify, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from crud-backend.Blog.routes import blogs
 
 app = Flask(__name__)
 CORS(app)
-
+app.register_blueprint(blogs)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@testdb:5432/keerthi'
 db = SQLAlchemy(app)
 
