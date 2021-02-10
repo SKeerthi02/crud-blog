afdcb2134308a6d7051f53297efec4eeb631990e
SKeerthi02 <keerthi.21094@gmail.com>
2021-02-10 16:25:12 -0500

    init postgres db and flask imports

--- crud-backend/app.py
+++ crud-backend/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, jsonify, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@testdb:5432/keerthi'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
