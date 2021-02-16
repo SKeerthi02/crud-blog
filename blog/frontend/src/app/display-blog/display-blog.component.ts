f94636a976e1527c6e9864d48c992633222a5d32
SKeerthi02 <keerthi.21094@gmail.com>
2021-02-16 17:44:26 -0500

    finishing display component

---
+++ blog/frontend/src/app/display-blog/display-blog.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Router } from '@angular/router';
+import {Blog} from '../models/blog';
+import {BlogService} from '../service/blog.service';
+
+@Component({
+  selector: 'app-display-blog',
+  templateUrl: './display-blog.component.html',
+  styleUrls: []
+})
+export class DisplayBlogComponent implements OnInit {
+  @Input() blog: Blog;
+
+  @Output() deleteEvent = new EventEmitter();
+
+  constructor(private router: Router, private blogService: BlogService) {}
+
+  ngOnInit() {}
+
+  public route(): void {
+    this.router.navigateByUrl('/blogs/' + this.blog.id);
+  }
+
+  public removeBlog() {
+    this.blogService.deleteBlog(this.blog.id).subscribe(res => {
+      this.deleteEvent.emit(this.blog);
+    });
+  }
+
+}
