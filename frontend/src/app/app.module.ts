7dead613967a242e8a314e427c1402466f4fff4b
SKeerthi02 <keerthi.21094@gmail.com>
2021-02-15 23:34:30 -0500

    adding navigation bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NewBlogComponent } from './new-blog/new-blog.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NewBlogComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
