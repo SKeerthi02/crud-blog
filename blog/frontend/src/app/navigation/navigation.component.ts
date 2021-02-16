9f689faf208cd8dc24100a96d72dd1b0073c857d
SKeerthi02 <keerthi.21094@gmail.com>
2021-02-16 18:19:35 -0500

    adding navbar component

---
+++ blog/frontend/src/app/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navigation.component.html',
+  styleUrls: []
+})
+export class NavigationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
