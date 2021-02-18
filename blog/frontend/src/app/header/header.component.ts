188ea25a9ebfb6841c18dcaacf1892fb518ec572
SKeerthi02 <keerthi.21094@gmail.com>
2021-02-18 03:14:46 -0500

    adding css to header component

--- blog/frontend/src/app/header/header.component.ts
+++ blog/frontend/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-masthead',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
